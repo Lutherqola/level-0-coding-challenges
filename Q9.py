72b72c52061504238352f40a5f15ee77fbee7546
Lutherqola <96020620+Lutherqola@users.noreply.github.com>
2022-01-27 01:07:18 +0200

    Update Q9.py

--- Q9.py
+++ Q9.py
@@ -3,7 +3,7 @@ def vowel_finder(word:str):
     print("Vowels:",end=' ')
     for x in word:
        if x in ["a","A","e","E","i","I","o","O","u","U"]:
-           lst.append(x)
+           lst.append(x.lower())
     print(','.join(lst))
               
 vowel_finder("PersuAsion")  
